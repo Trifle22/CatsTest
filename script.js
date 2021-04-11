bcc42059d1fd4c8be057b0b21cd0e7b885eee071
Trifle22 <xromchenkov99@gmail.com>
2021-04-12 01:21:49 +0300

    +catch

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ fetch(url, {
     'Content-Type': 'application/json; charset=utf-8',
     'x-api-Key': '2c90cc4c-aa92-459c-93ea-9e81a664c71d'
   }
-}).then(response => response.json()).then(response => renderCats(response));
+}).then(response => response.json()).then(response => renderCats(response)).catch(error => console.error(error));
 
 const renderCats = (response) => {
   response.forEach(item => {
